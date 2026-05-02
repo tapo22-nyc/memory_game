095c5a2c0ac0c5fc190cec10006aece9d80e79f0
tapo22-nyc <tapo.nyc@gmail.com>
2026-05-02 16:26:50 -0400

    Update get-ipl-leaderboard.js

--- api/get-ipl-leaderboard.js
+++ api/get-ipl-leaderboard.js
@@ -1,1 +1,16 @@
-SELECT * FROM ipl_leaderboard;
+const { neon } = require('@neondatabase/serverless');
+
+const sql = neon(process.env.DATABASE_URL);
+
+module.exports = async function handler(req, res) {
+  try {
+    const leaderboard = await sql`
+      SELECT * FROM ipl_leaderboard;
+    `;
+
+    res.status(200).json({ leaderboard });
+  } catch (error) {
+    console.error('leaderboard error:', error);
+    res.status(500).json({ error: error.message });
+  }
+};
