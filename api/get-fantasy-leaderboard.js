5a803cde3d0efaba0d63fa15ca5a8e7e50ccfe6d
tapo22-nyc <tapo.nyc@gmail.com>
2026-05-04 19:07:16 -0400

    Update get-fantasy-leaderboard.js

--- api/get-fantasy-leaderboard.js
+++ api/get-fantasy-leaderboard.js
@@ -4,25 +4,20 @@ const sql = neon(process.env.DATABASE_URL);
 
 module.exports = async function handler(req, res) {
   try {
-    const { fantasy_match_id } = req.query;
-
-    if (!fantasy_match_id) {
-      return res.status(400).json({ error: 'fantasy_match_id is required.' });
-    }
-
     const leaderboard = await sql`
       SELECT
         u.user_name,
         s.user_id,
-        s.base_points,
-        s.captain_bonus,
-        s.vice_captain_bonus,
-        s.total_points
+        SUM(s.total_points) AS total_points,
+        SUM(s.base_points) AS base_points,
+        SUM(s.captain_bonus) AS captain_bonus,
+        SUM(s.vice_captain_bonus) AS vice_captain_bonus,
+        COUNT(*) AS matches_played
       FROM fantasy_user_match_scores s
       JOIN ipl_users u
         ON u.id = s.user_id
-      WHERE s.fantasy_match_id = ${Number(fantasy_match_id)}
-      ORDER BY s.total_points DESC, s.base_points DESC
+      GROUP BY u.user_name, s.user_id
+      ORDER BY SUM(s.total_points) DESC
     `;
 
     return res.status(200).json({ leaderboard });
