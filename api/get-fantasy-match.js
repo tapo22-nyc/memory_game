50fd52604fb8b36b10eff1eac9b2f38dce2c8669
tapo22-nyc <tapo.nyc@gmail.com>
2026-05-04 16:30:41 -0400

    Update get-fantasy-match.js

--- api/get-fantasy-match.js
+++ api/get-fantasy-match.js
@@ -21,17 +21,30 @@ module.exports = async function handler(req, res) {
       LIMIT 1
     `;
 
+    if (!matches.length) {
+      return res.status(404).json({ error: 'Fantasy match not found.' });
+    }
+
     const players = await sql`
-      SELECT *
-      FROM fantasy_players
-      WHERE fantasy_match_id = ${Number(fantasy_match_id)}
-      ORDER BY team_name, player_name
+      SELECT
+        pm.id,
+        pm.player_name,
+        pm.team_code AS team_name,
+        pm.player_tag AS role,
+        pm.auction_price_cr,
+        pm.player_cost_coins
+      FROM fantasy_match_players fmp
+      JOIN ipl_player_master pm
+        ON pm.id = fmp.player_id
+      WHERE fmp.fantasy_match_id = ${Number(fantasy_match_id)}
+      ORDER BY pm.team_code, pm.player_name
     `;
 
     return res.status(200).json({
       match: matches[0],
       players
     });
+
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
