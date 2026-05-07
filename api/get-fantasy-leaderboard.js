672c24b011d0f39b683267262c761e4627b10830
tapo22-nyc <tapo.nyc@gmail.com>
2026-05-07 15:55:35 -0400

    Update get-fantasy-leaderboard.js

--- api/get-fantasy-leaderboard.js
+++ api/get-fantasy-leaderboard.js
@@ -4,21 +4,44 @@ const sql = neon(process.env.DATABASE_URL);
 
 module.exports = async function handler(req, res) {
   try {
-    const leaderboard = await sql`
-      SELECT
-        u.user_name,
-        s.user_id,
-        SUM(s.total_points) AS total_points,
-        SUM(s.base_points) AS base_points,
-        SUM(s.captain_bonus) AS captain_bonus,
-        SUM(s.vice_captain_bonus) AS vice_captain_bonus,
-        COUNT(*) AS matches_played
-      FROM fantasy_user_match_scores s
-      JOIN ipl_users u
-        ON u.id = s.user_id
-      GROUP BY u.user_name, s.user_id
-      ORDER BY SUM(s.total_points) DESC
-    `;
+    const fantasyMatchId = req.query.fantasy_match_id;
+
+    let leaderboard;
+
+    if (fantasyMatchId) {
+      leaderboard = await sql`
+        SELECT
+          u.user_name,
+          s.user_id,
+          s.fantasy_match_id,
+          s.total_points,
+          s.base_points,
+          s.captain_bonus,
+          s.vice_captain_bonus,
+          1 AS matches_played
+        FROM fantasy_user_match_scores s
+        JOIN ipl_users u
+          ON u.id = s.user_id
+        WHERE s.fantasy_match_id = ${Number(fantasyMatchId)}
+        ORDER BY s.total_points DESC
+      `;
+    } else {
+      leaderboard = await sql`
+        SELECT
+          u.user_name,
+          s.user_id,
+          SUM(s.total_points) AS total_points,
+          SUM(s.base_points) AS base_points,
+          SUM(s.captain_bonus) AS captain_bonus,
+          SUM(s.vice_captain_bonus) AS vice_captain_bonus,
+          COUNT(*) AS matches_played
+        FROM fantasy_user_match_scores s
+        JOIN ipl_users u
+          ON u.id = s.user_id
+        GROUP BY u.user_name, s.user_id
+        ORDER BY SUM(s.total_points) DESC
+      `;
+    }
 
     return res.status(200).json({ leaderboard });
   } catch (error) {
