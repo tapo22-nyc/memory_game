726fc11fc3b0f2ec1f023ce1a758fc0658f5d2c3
tapo22-nyc <tapo.nyc@gmail.com>
2026-05-16 21:18:16 -0400

    Update lock-fantasy-match.js

--- api/lock-fantasy-match.js
+++ api/lock-fantasy-match.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    const matchId = 54; // replace if different
+    const matchId = 64; // replace if different
 
     await sql`
       UPDATE fantasy_matches
